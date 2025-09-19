d7e3ad1cb24b9a8093041ab7d8195baa446667b6
sh_rajin <shrajin7@gmail.com>
2025-09-19 11:33:51 +0600

    mysql integrate

--- householdService_Api/settings.py
+++ householdService_Api/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 from pathlib import Path
 import environ
 env = environ.Env()
